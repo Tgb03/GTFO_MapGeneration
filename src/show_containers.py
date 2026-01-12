9c0c24c835adcab303eadeeef961d44e119a6c81
Tgb03 <69427849+Tgb03@users.noreply.github.com>
2026-01-12 23:15:03 +0100

    added customizable text size to show-containers

--- src/show_containers.py
+++ src/show_containers.py
@@ -47,13 +47,13 @@ def choose_marker(level_name):
             print("Please enter a number.")
 
 
-def add_text(svg, pos, bounds, text):
+def add_text(svg, pos, bounds, text, text_size):
     (pos_x, pos_y) = pos
 
     try:
         item_svg = load_item_svg("text")
         inner = extract_inner_svg(item_svg)
-        inner = inner.format(text=text)
+        inner = inner.format(text=text, size=str(text_size))
 
         pos_x, pos_y = to_svg_pos([pos_x, pos_y], bounds[0][0], bounds[1][1])
 
@@ -78,6 +78,7 @@ def main():
     parser.add_argument("-c", "--container-show", action="store_false", default=True, help="stop showing containers")
     parser.add_argument("-s", "--small-pickup-show", action="store_true", default=False, help="show small pickups")
     parser.add_argument("-b", "--big-pickup-show", action="store_true", default=False, help="show big pickups")
+    parser.add_argument("-t", "--text-size", type=float, nargs="?", default=1, help="change the text size")
 
     args = parser.parse_args()
     
@@ -87,6 +88,7 @@ def main():
     show_containers = args.container_show
     show_small_pickups = args.small_pickup_show
     show_big_pickups = args.big_pickup_show
+    text_size = args.text_size
 
     level_data = load_level(level_name, marker)
     if level_data is None:
@@ -114,7 +116,7 @@ def main():
     
                     svg = add_item(svg, name, pos, 0, bounds)
                     pos = (pos[0], pos[1] + 10)
-                    svg = add_text(svg, pos, bounds, str(id))
+                    svg = add_text(svg, pos, bounds, str(id), text_size)
 
         if show_small_pickups:
             for _, small_pickups_in_zone in small_pickups_map.get(i, {}).items():
@@ -124,7 +126,7 @@ def main():
     
                     svg = add_item(svg, name, pos, 0, bounds)
                     pos = (pos[0], pos[1] + 10)
-                    svg = add_text(svg, pos, bounds, str(id))
+                    svg = add_text(svg, pos, bounds, str(id), text_size)
 
         if show_big_pickups:
             for _, big_pickups_in_zone in big_pickups_map.get(i, {}).items():
@@ -134,7 +136,7 @@ def main():
     
                     svg = add_item(svg, name, pos, 0, bounds)
                     pos = (pos[0], pos[1] + 10)
-                    svg = add_text(svg, pos, bounds, str(id))
+                    svg = add_text(svg, pos, bounds, str(id), text_size)
 
         open_generated_svg(svg)
 
